b824ed0256ad48b4328a9a5683f7967adea74330
sircharlesdev <46936720+sircharlesdev@users.noreply.github.com>
2020-11-01 13:01:24 -0300

    Add files via upload

---
+++ buclefor.py
@@ -0,0 +1,8 @@
+for i in range(3,90,3):
+    print(f"Valor de la variable {i}")
+
+longitud=len("juan")
+
+for j in range(longitud):
+    print(j)
+
---
+++ clase18_bucles.py
@@ -0,0 +1,11 @@
+nombre="Pildoras informaticas"
+
+contador=0
+
+for i in nombre:
+	if i==" ":
+		continue
+	contador+=1
+
+print(contador)
+
